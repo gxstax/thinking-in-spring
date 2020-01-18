e7cc6725bdb2a1abaa79fd6db99c3ce9a536e810
ant <gxstax@163.com>
2020-01-18 23:29:21 +0800

    spring bean 实例化方式

---
+++ spring-bean/src/main/java/com/ant/spring/bean/factory/UserFactory.java
@@ -0,0 +1,18 @@
+package com.ant.spring.bean.factory;
+
+import com.ant.spring.ioc.overview.domain.User;
+
+/**
+ * <p>
+ * {@link com.ant.spring.ioc.overview.domain.User} User 工厂类
+ * </p>
+ *
+ * @Author Ant gxstax@163.com
+ * @since 2020/1/18 11:07 下午
+ **/
+public interface UserFactory {
+
+    default User createUser() {
+       return User.createUser();
+    }
+}
