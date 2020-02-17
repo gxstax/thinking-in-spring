3ee84b8e8bf3ea50fbabb198e25877cc6eeeb4d4
gaoxin <gaoxx@heytea.com>
2020-02-17 09:11:05 +0800

    spring 依赖查找

--- dependency-lookup/src/main/java/com/ant/spring/dependency/lookup/ObjectProviderDemo.java
+++ dependency-lookup/src/main/java/com/ant/spring/dependency/lookup/ObjectProviderDemo.java
@@ -1,8 +1,12 @@
 package com.ant.spring.dependency.lookup;
 
+import com.ant.spring.ioc.overview.domain.User;
 import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
+
+import java.util.Iterator;
 
 /**
  * <p>
@@ -23,14 +27,43 @@ public class ObjectProviderDemo {
 
         lookupByObjectProvider(context);
 
+        lookupIfAvaIfAvailable(context);
+
+        lookupByStreamOps(context);
+
+
         // 关闭 Spring 应用上下文
         context.close();
-        System.gc();
+    }
+
+    private static void lookupByStreamOps(AnnotationConfigApplicationContext context) {
+        ObjectProvider<String> beanProvider = context.getBeanProvider(String.class);
+//        Iterable<String> iterable = beanProvider;
+//        for (String s : iterable) {
+//            System.out.println(s);
+//        }
+        beanProvider.stream().forEach(System.out::println);
+
+    }
+
+    private static void lookupIfAvaIfAvailable(AnnotationConfigApplicationContext context) {
+        ObjectProvider<User> beanProvider = context.getBeanProvider(User.class);
+        // 如果上下文中没有user对象，我们这里进行一个兜底操作，新建一个User对象
+        User user = beanProvider.getIfAvailable(User::createUser);
+
+        System.out.println("当前的 User 对象: " + user);
+
     }
 
     @Bean
+    @Primary
     public String helloWorld() {
         return "Hello,World";
+    }
+
+    @Bean
+    public String message() {
+        return "Message";
     }
 
     private static void lookupByObjectProvider(AnnotationConfigApplicationContext context) {
